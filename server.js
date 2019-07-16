50d0a3c02e47223c2805c1d6d864edf51e2e988f
Jemila <jemila.abulhawa@gmail.com>
2019-07-16 09:02:48 +0300

    set up mongoose connection - add config.env to gitignore

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose')
 const app = require('./app')
 const env = require('dotenv')
 
 env.config({ path: './config.env' })
+
+const DB = process.env.DB_URL.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+)
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'))
+  .catch(err => console.log(err))
 
 const port = process.env.PORT || 3000
 
